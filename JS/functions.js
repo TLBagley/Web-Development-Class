cc49b7648bda69c523cc7547e04e9ccd7ec4aa54
TLBagley <tlbagley@gmail.com>
2016-04-19 21:58:03 -0600

    latest revision
    Looking good

--- JS/functions.js
+++ JS/functions.js
@@ -14,4 +14,16 @@ function SendSubmitContactForm()
 //  window.location.href = "mailto:TLBagley@Gmail.com?subject="
 //        + encodeURIComponent("Contact")
 //        + "&body=" + encodeURIComponent(comment);
+
+}
+
+
+function enableEasterEgg()
+{
+
+  var egg = new Egg("up,up,down,down,left,right,left,right,b,a", function() {
+    jQuery('#egggif').fadeIn(500, function() {
+      window.setTimeout(function() { jQuery('#egggif').hide(); }, 5000);
+    });
+  }).listen();
 }
